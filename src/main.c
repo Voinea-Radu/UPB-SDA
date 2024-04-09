d9c7ac8fc357904e9a73d591bfd9b832781cac47
Voinea Radu <contact@voinearadu.com>
2024-04-09 07:24:42 +0300

    Further small changes to structure

--- src/main.c
+++ src/main.c
@@ -30,91 +30,85 @@ void read_quoted_string(string_t buffer, int buffer_len, int *start, int *end)
 	}
 }
 
-request_type_t read_request_arguments(FILE *input_file, string_t buffer,
-									  int *maybe_server_id, int *maybe_cache_size,
-									  char **maybe_doc_name, char **maybe_doc_content)
+request_type_t read_request_arguments(FILE *input_file, string_t buffer, int *optional_server_id, int *optional_cache_size,
+									  string_t *optional_document_name, string_t *optional_document_content)
 {
-	request_type_t req_type;
+	request_type_t request_type;
 	int word_start = -1;
 	int word_end = -1;
 
 	check_or_exit(fgets(buffer, REQUEST_LENGTH + 1, input_file) == NULL, "Insufficient requests");
 
-	req_type = get_request_type(buffer);
+	request_type = get_request_type(buffer);
 
-	if (req_type == ADD_SERVER) {
-		*maybe_server_id = atoi(buffer + strlen(ADD_SERVER_REQUEST) + 1);
-		*maybe_cache_size = atoi(strchr(
-				buffer + strlen(ADD_SERVER_REQUEST) + 1, ' '));
-	} else if (req_type == REMOVE_SERVER) {
-		*maybe_server_id = atoi(buffer + strlen(REMOVE_SERVER_REQUEST) + 1);
+	if (request_type == ADD_SERVER) {
+		*optional_server_id = atoi(buffer + strlen(ADD_SERVER_REQUEST) + 1);
+		*optional_cache_size = atoi(strchr(buffer + strlen(ADD_SERVER_REQUEST) + 1, ' '));
+	} else if (request_type == REMOVE_SERVER) {
+		*optional_server_id = atoi(buffer + strlen(REMOVE_SERVER_REQUEST) + 1);
 	} else {
-		*maybe_doc_name = safe_calloc(DOC_NAME_LENGTH + 1);
+		*optional_document_name = safe_calloc(DOC_NAME_LENGTH + 1);
 
 		read_quoted_string(buffer, REQUEST_LENGTH, &word_start, &word_end);
-		memcpy(*maybe_doc_name, buffer + word_start + 1,
-			   word_end - word_start - 1);
+		memcpy(*optional_document_name, buffer + word_start + 1, word_end - word_start - 1);
 
-		if (req_type == EDIT_DOCUMENT) {
+		if (request_type == EDIT_DOCUMENT) {
 			char *tmp_buffer = buffer + word_end + 1;
 
-			*maybe_doc_content = safe_calloc(DOC_CONTENT_LENGTH + 1);
+			*optional_document_content = safe_calloc(DOC_CONTENT_LENGTH + 1);
 
 			/* Read the content, which might be a multiline quoted string */
 			word_start = -1;
-			read_quoted_string(tmp_buffer, DOC_CONTENT_LENGTH,
-							   &word_start, &word_end);
+			read_quoted_string(tmp_buffer, DOC_CONTENT_LENGTH, &word_start, &word_end);
 
-			if (word_end == -1)
-				strcpy(*maybe_doc_content, tmp_buffer + word_start + 1);
-			else
-				strncpy(*maybe_doc_content, tmp_buffer + word_start + 1,
-						word_end - word_start - 1);
+			if (word_end == -1) {
+				strcpy(*optional_document_content, tmp_buffer + word_start + 1);
+			} else {
+				strncpy(*optional_document_content, tmp_buffer + word_start + 1, word_end - word_start - 1);
+			}
 
 			while (word_end == -1) {
 				check_or_exit(fgets(buffer, DOC_CONTENT_LENGTH + 1, input_file) == NULL, "Document content is not properly quoted.");
 
-				read_quoted_string(buffer, DOC_CONTENT_LENGTH,
-								   &word_start, &word_end);
-				memcpy(*maybe_doc_content + strlen(*maybe_doc_content), buffer,
-					   word_end == -1 ? strlen(buffer) : (unsigned)word_end);
+				read_quoted_string(buffer, DOC_CONTENT_LENGTH, &word_start, &word_end);
+				memcpy(*optional_document_content + strlen(*optional_document_content), buffer, word_end == -1 ? strlen(buffer) : (unsigned)word_end);
 			}
 		} else {
-			*maybe_doc_content = NULL;
+			*optional_document_content = NULL;
 		}
 	}
 
-	return req_type;
+	return request_type;
 }
 
 void apply_requests(FILE *input_file, char *buffer,
 					int requests_num, bool enable_vnodes)
 {
-	char *doc_name, *doc_content;
+	string_t document_name;
+	string_t document_content;
 	int server_id, cache_size;
 
-	load_balancer_t *main = load_balancer_init(enable_vnodes);
+	load_balancer_t *load_balancer = load_balancer_init(enable_vnodes);
 
 	for (int i = 0; i < requests_num; i++) {
-		request_type_t req_type = read_request_arguments(input_file, buffer,
-														 &server_id, &cache_size, &doc_name, &doc_content);
+		request_type_t request_type = read_request_arguments(input_file, buffer, &server_id, &cache_size, &document_name, &document_content);
 
-		if (req_type == ADD_SERVER) {
+		if (request_type == ADD_SERVER) {
 			check_or_exit(cache_size < 0, "Cache size must be positive.");
-			load_balancer_add_server(main, server_id, cache_size);
-		} else if (req_type == REMOVE_SERVER) {
-			load_balancer_remove_server(main, server_id);
+			load_balancer_add_server(load_balancer, server_id, cache_size);
+		} else if (request_type == REMOVE_SERVER) {
+			load_balancer_remove_server(load_balancer, server_id);
 		} else {
 			request_t server_request = {
-					.type = req_type,
-					.document_name = doc_name,
+					.type = request_type,
+					.document_name = document_name,
 			};
 
-			if (req_type == EDIT_DOCUMENT) {
-				server_request.document_content = doc_content;
+			if (request_type == EDIT_DOCUMENT) {
+				server_request.document_content = document_content;
 			}
 
-			response_t *response = load_balancer_forward_request(main, &server_request);
+			response_t *response = load_balancer_forward_request(load_balancer, &server_request);
 
 			free(server_request.document_name);
 			free(server_request.document_content);
@@ -123,7 +117,7 @@ void apply_requests(FILE *input_file, char *buffer,
 		}
 	}
 
-	load_balancer_free(&main);
+	load_balancer_free(&load_balancer);
 }
 
 int main(int argc, string_t *argv)
